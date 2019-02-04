279f24130d6e63c5291ad17be1659d7b71071af1
David DeGraw <david.degraw@gmail.com>
2019-02-03 19:09:25 -0700

    Clock and raw data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Web MIDI Playground</h3>
+    <template v-if="!clockInterval">
+      <input v-model="clockTempo" type="number" placeholder="BPM"/>
+      <button
+        @click="startMidiCLock"
+      >Start Clock</button>
+    </template>
+    <template v-else>
+      <button
+        @click="stopMidiClock"
+      >Stop Clock</button>
+    </template>
+
+    <br>
+
+    <input v-model="midiData.status" type="number" placeholder="Status"/>
+    <input v-model="midiData.data1" type="number" placeholder="Data Byte 1"/>
+    <input v-model="midiData.data2" type="number" placeholder="Data Byte 2"/>
+    <button
+      @click="sendMidiByte"
+    >Send Byte</button>
+
+    <p>Built with <a href="https://vuejs.org/">Vue.js</a></p>
+    <p>and <a href="https://github.com/djipco/webmidi">WebMidi.js</a></p>
+    <p>by <a href="https://github.com/catskull">catskull</a></p>
+    <p>2019</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WebMidi from 'webmidi'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      outputDevice: {},
+      clockTempo: undefined,
+      clockInterval: undefined,
+      midiData: {
+        status: null,
+        data1: null,
+        data2: null
+      }
+    }
+  },
+  computed: {
+    bpmInMilliseconds () {
+      console.log(parseInt(this.clockTempo))
+      return 60000 / parseInt(this.clockTempo)
+    }
+  },
+  methods: {
+    startMidiCLock () {
+      this.clockInterval = setInterval(() => {
+        this.sendMidiClock()
+      }, this.bpmInMilliseconds)
+    },
+    stopMidiClock () {
+      clearInterval(this.clockInterval)
+      this.clockInterval = undefined
+    },
+    sendMidiClock () {
+      this.outputDevice.sendClock()
+    },
+    sendMidiByte () {
+      this.outputDevice.send(this.midiData.status, [this.midiData.data1, this.midiData.data2])
+    }
+  },
+  created () {
+    WebMidi.enable(err => {
+      if (err) {
+        console.log('WebMidi not enabled', err)
+      } else {
+        this.outputDevice = WebMidi.outputs[0]
+        this.outputDevice.sendStart()
+        this.outputDevice.playNote(64, 1)
+        this.outputDevice.sendClock()
+      }
+    }, 'enable sysex')
   }
 }
 </script>
